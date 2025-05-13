3029952a7efa8fcc59fc005a8795296fd4e3baa2
shapiro-gis <62624305+shapiro-gis@users.noreply.github.com>
2025-05-12 18:49:05 -0600

    Update extract annual raster stacks

--- R/ExtractAnnualDAYMET.R
+++ R/ExtractAnnualDAYMET.R
@@ -24,63 +24,77 @@
 #' 
 
 
-ExtractAnnualDAYMET <- function(point_data, 
-                                start_date = "2005-01-01", 
-                                end_date = "2010-01-01", 
-                                metric_name = NULL) {
+ExtractAnnualDAYMET <- function(XYdata, datesname = "date", Metrics = c("prcp", "swe"),
+                                num_cores = NULL) {
+  if (!inherits(XYdata, "sf"))
+    stop("XYdata must be an sf object")
   
-  # Load necessary libraries
+  allowed_metrics <- c("prcp", "swe", "tmax", "tmin")
+  
+  Metrics <- tolower(Metrics)
+  if (!all(Metrics %in% allowed_metrics)) {
+    stop(paste0("Metrics must be one or more of: ", paste(allowed_metrics, collapse = ", ")))
+  }
+
+  
   require(terra)
   require(sf)
   require(httr)
   require(jsonlite)
   require(tidyverse)
+  require(lubridate)
+  require(parallel)
   
-  if(!inherits(point_data, "sf")) {
-    stop("point_data must be an sf object")
-  }
-  # Obtain the list of all available metrics
-  myDaymetMetrics <- httr::POST("https://devise.uwyo.edu/umbraco/api/daymetapi/GetDerivedAnnualMetricsList", content_type_json()) %>% content()
-  myDaymetMetrics <- do.call(rbind.data.frame, myDaymetMetrics)
+  if (is.null(num_cores)) num_cores <- detectCores() - 1
   
-  if(any(metric_name %in% c("Maxprcp","Maxswe","Maxtmax","Maxtmin","Meanprcp","Meanswe","Meantmax","Meantmin","Medianprcp","Medianswe","Mediantmax","Mediantmin")==FALSE)){
-    stop("The DAYMET metric must only be Maxprcp,Maxswe,Maxtmax,Maxtmin,Meanprcp,Meanswe,
-    Meantmax,Meantmin,Medianprcp,Medianswe,Mediantmax,Mediantmin")
-  }
+  dates <- XYdata[[datesname]]
+  if (!inherits(dates, "POSIXct")) stop("Your dates column is not POSIXct")
+  if (any(is.na(dates))) stop("NA values found in date column")
   
-  # Select metric
-  if(is.null(metric_name)) {
-    metric <- myDaymetMetrics$metric[1]
-  } else {
-    metric <- metric_name
-  }
+  XYdata$year <- year(dates)
   
-  # Get the data
-  dat <- httr::POST("https://devise.uwyo.edu/umbraco/api/Daymetapi/GetDerivedAnnualData", 
-                    httr::content_type_json(),
-                    body = jsonlite::toJSON(list(StartDate = jsonlite::unbox(start_date),
-                                                 EndDate = jsonlite::unbox(end_date),
-                                                 Metrics = metric),
-                                            auto_unbox = FALSE)
-  ) %>% content()
-  dat <- do.call(rbind.data.frame, dat)
+  original_crs <- st_crs(XYdata)
+  XYdata <- st_transform(XYdata, 5072)
   
-  # Read rasters directly from URL
-  rasters <- lapply(dat$url, function(url) terra::rast(url))
+  clust <- makeCluster(num_cores)
+  clusterEvalQ(clust, {
+    library(terra)
+    library(sf)
+  })
+  clusterExport(clust, varlist = c("XYdata", "Metrics"), envir = environment())
   
-  # Combine the rasters
-  rs <- do.call(c, rasters)
+  result_list <- clusterApplyLB(clust, 1:nrow(XYdata), function(i) {
+    row <- XYdata[i, ]
+    values <- sapply(Metrics, function(metric) {
+      yr <- row$year
+      
+      url <- sprintf("https://pathfinder.arcc.uwyo.edu/devise/cloudenabled/annual/cog/daymet/daymet_annual_%s_all-years.tif",
+                     metric)
+      vsicurl_path <- paste0("/vsicurl/", url)
+      
+      tryCatch({
+        r <- terra::rast(vsicurl_path)
+        band_names <- names(r)
+        band_index <- which(grepl(paste0(metric, "_", yr), band_names))
+        
+        if (length(band_index) == 0) return(NA)
+        
+        v <- terra::extract(r[[band_index]], row)
+        return(v[1, 2])
+      }, error = function(e) {
+        return(NA)
+      })
+    }, simplify = FALSE)
+    
+    return(as.data.frame(values))
+  })
   
-  names(rs) <- str_split(dat$filename, "\\.", simplify = TRUE)[,1]
+  stopCluster(clust)
   
-  pts <- st_transform(point_data, crs = 5072)
- #pts <- vect(as(points %>% st_transform(crs = 5072), "Spatial"))
+  result_df <- do.call(rbind, result_list)
+  colnames(result_df) <- Metrics
   
-  # Extract data
-  myMaxPrcpData <- terra::extract(rs, pts, ID = FALSE)
-  df <- as.data.frame(myMaxPrcpData)
-  names(df) <- names(rs)
-  result <- cbind(point_data, df)
-  
-  return(result)
+  XYdata <- cbind(XYdata, result_df)
+  XYdata <- st_transform(XYdata, crs = original_crs)
+  return(XYdata)
 }
